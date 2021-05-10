ac32d6e7cd865bccaa021994b563600ae8dccf1e
Automaton <46001539+automatonatm@users.noreply.github.com>
2021-05-10 14:07:29 +0100

    Update campaign.sol

--- campaign.sol
+++ campaign.sol
@@ -8,8 +8,11 @@ contract Campaign {
     struct Request {
         string description;
         uint value;
-        address recipient;
+        address payable recipient;
         bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
+        
     }
     
     Request[] public requests;
@@ -17,6 +20,7 @@ contract Campaign {
     uint public minimumContribution;
    // address[] public approvers;
    mapping(address => bool) public approvers;
+   uint public approversCount;
     
     
     
@@ -35,21 +39,48 @@ contract Campaign {
         require(msg.value > minimumContribution);
         //approvers.push(msg.sender);
         approvers[msg.sender] = true;
+        approversCount++;
     }
     
     
-    function createRequest(string memory description, uint  value, address recipient) public restricted {
+    function createRequest(string memory description, uint  value, address  payable recipient) public restricted {
         
         Request memory newRequest = Request({
             description: description,
             value: value,
             recipient: recipient,
-            complete: false
+            complete: false,
+            approvalCount: 0
         });
         
         requests.push(newRequest);
     } 
     
+    function approveRequest(uint index) public {
+        
+        Request storage request = requests[index];
+        
+        require(approvers[msg.sender]); //check if the contributor exist
+        require(!request.approvals[msg.sender]);
+        
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+        
+    }
+    
+    
+    function finalizeRequest(uint index) public restricted {
+        
+        Request storage request = requests[index];
+        
+        require(request.approvalCount > (approversCount / 2));
+        require(!request.complete);
+        
+        request.recipient.transfer(request.value);
+    
+        request.complete = true;
+        
+    }
        
     
     
